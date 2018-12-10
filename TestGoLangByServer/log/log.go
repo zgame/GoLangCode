796893da50030da4bc1598117eba2234b12dc8d5
朱世伟 <zgamecn@gmail.com>
2018-12-10 10:49:09 +0800

    add
    add

--- TestGoLangByServer/log/log.go
+++ TestGoLangByServer/log/log.go
@@ -4,10 +4,46 @@ import (
 	"os"
 	"log"
 	"fmt"
+	"time"
 )
+
+
+
+
+// 判断文件夹是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
 // 记录，并且显示，字符串
 func PrintLogger(s string, print bool)  {
-	file, _ := os.OpenFile("Log.log",os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModeAppend|os.ModePerm)
+	logDir := "Logs"
+	exist, err := PathExists(logDir)
+	if err != nil {
+		fmt.Printf("get Logs/ dir error![%v]\n", err)
+		return
+	}
+	if !exist {
+		err := os.Mkdir(logDir, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir %s failed![%v]\n", logDir, err)
+		} else {
+			fmt.Printf("mkdir %s success!\n", logDir)
+		}
+	}
+
+	// 根据日期进行分文件
+	t1:=time.Now()
+	t11 := t1.Format("2006-01-02")
+
+	file, _ := os.OpenFile(logDir+"/Logger_"+t11 +".log",os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModeAppend|os.ModePerm)
 	logger := log.New(file, "", log.LstdFlags)
 	logger.Println("[Log] ",s)
 	if print {
