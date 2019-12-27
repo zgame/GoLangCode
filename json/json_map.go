4539e70e99e7a9afcd1a2e47c3a1f55f0cabfadb
朱世伟 <zgamecn@gmail.com>
2019-12-27 13:32:37 +0800

    add

---
+++ json/json_map.go
@@ -0,0 +1,52 @@
+package main
+
+
+import (
+	"fmt"
+	"encoding/json"
+	"reflect"
+)
+
+type TestS struct {
+	IP string
+	Name string
+	ID  float32
+}
+
+
+
+func main() {
+	var test1 TestS
+
+	b := []byte(`{"IP": "127.0.0.1", "name": "SKY","ID":1.2}`)
+
+	m := make(map[string]interface{})
+
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println("Umarshal failed:", err)
+		return
+	}
+	err = json.Unmarshal(b, &test1)
+	if err != nil {
+		fmt.Println("Umarshal failed:", err)
+		return
+	}
+
+	fmt.Println("m:", m)
+	fmt.Println("m.IP:", reflect.TypeOf(m["IP"]))
+	fmt.Println("m.ID:", reflect.TypeOf(m["ID"]))
+
+	fmt.Println("--------------------------------------------")
+	fmt.Println("--------------------------------------------")
+	fmt.Println("test1:", test1)
+	fmt.Println("test1.IP:", test1.IP)
+
+
+	fmt.Println("--------------------------------------------")
+	fmt.Println("--------------------------------------------")
+	for k, v := range m {
+		fmt.Println(k, ":", v)
+	}
+
+}
