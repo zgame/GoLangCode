1d0b88a073422c985a444711e16a907ef05113c2
朱世伟 <zgamecn@gmail.com>
2020-12-17 16:50:23 +0800

    add

--- GoLuaServerV2.1/Script/Const/constCmdGame.lua
+++ GoLuaServerV2.1/Script/Const/constCmdGame.lua
@@ -1,7 +1,9 @@
 
 
 CMD_CCC ={
-    SUB_LOGON = 2  ,             -- 登陆命令Proto_Game_CCC.GameLogin() , 登录结果Proto_Game_CCC.GameLoginResult()
+    SUB_LOGON = 2  ,             -- 登陆命令GameLogin() , 登录结果GameLoginResult()
+    SUB_OTHER_LOGON = 3  ,       -- 其他玩家登陆命令OtherEnterRoom()
+    SUB_LOCATION =4 ,            -- 上报位置信息PlayerLocation(),   同步其他玩家位置PlayerLocation()
 
 
 }
---
+++ GoLuaServerV2.1/Script/copy.bat
@@ -0,0 +1,11 @@
+copy Const\constCmd.lua D:\work\SandrockMulitPlayer\Project\trunk\Cmd
+copy Const\constCmdGame.lua D:\work\SandrockMulitPlayer\Project\trunk\Cmd
+
+
+
+copy Protocol\build\*.lua    D:\work\SandrockMulitPlayer\Project\trunk\Protocol
+copy Protocol\build\*.proto    D:\work\SandrockMulitPlayer\Project\trunk\Protocol
+copy Protocol\build\*.txt    D:\work\SandrockMulitPlayer\Project\trunk\Protocol
+copy Protocol\build\build.bat    D:\work\SandrockMulitPlayer\Project\trunk\Protocol
+
+pause
