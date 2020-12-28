45eb05c25dde853e99acc91315b51b304386e106
朱世伟 <zgamecn@gmail.com>
2020-12-28 16:27:20 +0800

    add

--- GoLuaServerV2.1/Script/Protocol/build/build.bat
+++ GoLuaServerV2.1/Script/Protocol/build/build.bat
@@ -1,5 +1,5 @@
 
-protoc -o protoGameSandRock.pb.txt protoGameSandRock.proto
+rem protoc -o protoGameSandRock.pb.txt protoGameSandRock.proto
 protoc --plugin=protoc-gen-lua="plugin\protoc-gen-lua.bat" --lua_out=. protoGameSandRock.proto
 protoc --plugin=protoc-gen-lua="plugin\protoc-gen-lua.bat" --lua_out=. protoServer.proto
 
--- GoLuaServerV2.1Test/Script/Protocol/build/proto.lua
+++ GoLuaServerV2.1Test/Script/Protocol/build/proto.lua
@@ -1,5 +1,6 @@
 
 
+
 ProtoGameSandRock = require("protoGameSandRock_pb")
 ProtoServer = require("protoServer_pb")
 
